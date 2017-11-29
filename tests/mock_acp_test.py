3a174f42c9225802a0ed5a53509109a8d3375df1
david-wirelab <info@wirelab.co.uk>
2017-11-29 11:55:04 +0000

    cidr fix

--- tests/mock_acp_test.py
+++ tests/mock_acp_test.py
@@ -38,11 +38,11 @@ class TestE2E(unittest.TestCase):
     def test_root_destroy(self):
         self.assertEqual(self.result["destroy"], False)
 
-    def test_vpc_acpcicd_vpc_cidr_block(self):
-        self.assertEqual(self.result['mock-acp']["aws_vpc.acpcicdvpc"]["acpcicd_cidr_block"], "10.7.0.0/16")
+    def test_acpcicd_vpc_cidr_block(self):
+        self.assertEqual(self.result['mock-acp']["aws_vpc.acpcicdvpc"]["cidr_block"], "10.7.0.0/16")
 
-    def test_subnet_acpcicd_subnet_cidr_block(self):
-        self.assertEqual(self.result['mock-acp']["aws_subnet.ACPCICDSubnet"]["acpcicd_vpc_subnet_cidr_block"], "10.7.1.0/24")
+    def test_acpcicd_subnet_cidr_block(self):
+        self.assertEqual(self.result['mock-acp']["aws_subnet.ACPCICDSubnet"]["cidr_block"], "10.7.1.0/24")
 
     def test_az(self):
         self.assertEqual(self.result['mock-acp']["aws_subnet.ACPCICDSubnet"]["availability_zone"], "eu-west-2a")
@@ -57,76 +57,76 @@ class TestE2E(unittest.TestCase):
         self.assertEqual(self.result['mock-acp']["aws_security_group.BastionsACPCICD"]["tags.Name"], "dq-acpcicd-sg")
 
     def test_name_prefix_acpcicdvpc(self):
-        self.assertEqual(self.result['acpcicdvpc']["aws_vpc.acpcicdvpc"]["tags.Name"], "dq-acpcicd-vpc")
+        self.assertEqual(self.result['mock-acp']["aws_vpc.acpcicdvpc"]["tags.Name"], "dq-acpcicd-vpc")
 
 #### ACPOPS
-#
-#     def test_vpc_acpopscidr_block(self):
-#         self.assertEqual(self.result['acpops']["aws_vpc.acpopsvpc"]["acpops_cidr_block"], "10.6.0.0/16")
-#
-#     def test_subnet_acpops_cidr_block(self):
-#         self.assertEqual(self.result['acpops']["aws_subnet.ACPOPSSubnet"]["acpops_vpc_subnet_cidr_block"], "10.6.1.0/24")
-#
-#     def test_az(self):
-#         self.assertEqual(self.result['acpops']["aws_subnet.ACPOPSSubnet"]["availability_zone"], "eu-west-2a")
-#
-#     def test_name_prefix_BastionHostLinuxACPOPS(self):
-#         self.assertEqual(self.result['acpops']["aws_instance.BastionHostLinuxACPOPS"]["tags.Name"], "dq-acpops-ec2-linux")
-#
-#     def test_name_prefix_ACPOPSRouteToInternet(self):
-#         self.assertEqual(self.result['acpops']["aws_internet_gateway.ACPOPSRouteToInternet"]["tags.Name"], "dq-acpops-igw")
-#
-#     def test_name_prefix_BastionsACPOPS(self):
-#         self.assertEqual(self.result['acpops']["aws_security_group.Bastions"]["tags.Name"], "dq-acpops-sg")
-#
-#     def test_name_prefix_acpopsvpc(self):
-#         self.assertEqual(self.result['acpops']["aws_vpc.acpopsvpc"]["tags.Name"], "dq-acpops-vpc")
-#
+
+    def test_vpc_acpopscidr_block(self):
+        self.assertEqual(self.result['mock-acp']["aws_vpc.acpopsvpc"]["cidr_block"], "10.6.0.0/16")
+
+    def test_subnet_acpops_cidr_block(self):
+        self.assertEqual(self.result['mock-acp']["aws_subnet.ACPOPSSubnet"]["cidr_block"], "10.6.1.0/24")
+
+    def test_az(self):
+        self.assertEqual(self.result['mock-acp']["aws_subnet.ACPOPSSubnet"]["availability_zone"], "eu-west-2a")
+
+    def test_name_prefix_BastionHostLinuxACPOPS(self):
+        self.assertEqual(self.result['mock-acp']["aws_instance.BastionHostLinuxACPOPS"]["tags.Name"], "dq-acpops-ec2-linux")
+
+    def test_name_prefix_ACPOPSRouteToInternet(self):
+        self.assertEqual(self.result['mock-acp']["aws_internet_gateway.ACPOPSRouteToInternet"]["tags.Name"], "dq-acpops-igw")
+
+    def test_name_prefix_BastionsACPOPS(self):
+        self.assertEqual(self.result['mock-acp']["aws_security_group.BastionsACPOPS"]["tags.Name"], "dq-acpops-sg")
+
+    def test_name_prefix_acpopsvpc(self):
+        self.assertEqual(self.result['mock-acp']["aws_vpc.acpopsvpc"]["tags.Name"], "dq-acpops-vpc")
+
 # #### ACPPROD
-#
-#     def test_vpc_acpprod_cidr_block(self):
-#         self.assertEqual(self.result['acpprod']["aws_vpc.acpprodvpc"]["acpprod_cidr_block"], "10.5.0.0/16")
-#
-#     def test_subnet_acpprod_cidr_block(self):
-#         self.assertEqual(self.result['acpprod']["aws_subnet.ACPPRODSubnet"]["acpprod_vpc_subnet_cidr_block"], "10.5.1.0/24")
-#
-#     def test_az(self):
-#         self.assertEqual(self.result['acpprod']["aws_subnet.ACPPRODSubnet"]["availability_zone"], "eu-west-2a")
-#
-#     def test_name_prefix_BastionHostLinuxACPPROD(self):
-#         self.assertEqual(self.result['acpprod']["aws_instance.BastionHostLinuxACPPROD"]["tags.Name"], "dq-acpprod-ec2-linux")
-#
-#     def test_name_prefix_ACPPRODRouteToInternet(self):
-#         self.assertEqual(self.result['acpprod']["aws_internet_gateway.ACPPRODRouteToInternet"]["tags.Name"], "dq-acpprod-igw")
-#
-#     def test_name_prefix_BastionsACPPROD(self):
-#         self.assertEqual(self.result['acpprod']["aws_security_group.BastionsACPPROD"]["tags.Name"], "dq-acpprod-sg")
-#
-#     def test_name_prefix_acpprodvpc(self):
-#         self.assertEqual(self.result['acpprod']["aws_vpc.acpprodvpc"]["tags.Name"], "dq-acpprod-vpc")
-#
+
+    def test_vpc_acpprod_cidr_block(self):
+        self.assertEqual(self.result['mock-acp']["aws_vpc.acpprodvpc"]["cidr_block"], "10.5.0.0/16")
+
+    def test_subnet_acpprod_cidr_block(self):
+        self.assertEqual(self.result['mock-acp']["aws_subnet.ACPPRODSubnet"]["cidr_block"], "10.5.1.0/24")
+
+    def test_az(self):
+        self.assertEqual(self.result['mock-acp']["aws_subnet.ACPPRODSubnet"]["availability_zone"], "eu-west-2a")
+
+    def test_name_prefix_BastionHostLinuxACPPROD(self):
+        self.assertEqual(self.result['mock-acp']["aws_instance.BastionHostLinuxACPPROD"]["tags.Name"], "dq-acpprod-ec2-linux")
+
+    def test_name_prefix_ACPPRODRouteToInternet(self):
+        self.assertEqual(self.result['mock-acp']["aws_internet_gateway.ACPPRODRouteToInternet"]["tags.Name"], "dq-acpprod-igw")
+
+    def test_name_prefix_BastionsACPPROD(self):
+        self.assertEqual(self.result['mock-acp']["aws_security_group.BastionsACPPROD"]["tags.Name"], "dq-acpprod-sg")
+
+    def test_name_prefix_acpprodvpc(self):
+        self.assertEqual(self.result['mock-acp']["aws_vpc.acpprodvpc"]["tags.Name"], "dq-acpprod-vpc")
+
 # #### ACPVPN
-#
-#     def test_vpc_apcvpn_cidr_block(self):
-#         self.assertEqual(self.result['acpvpn']["aws_vpc.acpvpnvpc"]["acpvpn_cidr_block"], "10.4.0.0/16")
-#
-#     def test_subnet_acpvpn_cidr_block(self):
-#         self.assertEqual(self.result['acpvpn']["aws_subnet.ACPVPNSubnet"]["acpvpn_vpc_subnet_cidr_block"], "10.4.1.0/24")
-#
-#     def test_az(self):
-#         self.assertEqual(self.result['acpvpn']["aws_subnet.ACPVPNSubnet"]["availability_zone"], "eu-west-2a")
-#
-#     def test_name_prefix_BastionHostLinuxACPVPN(self):
-#         self.assertEqual(self.result['acpvpn']["aws_instance.BastionHostLinuxACPVPN"]["tags.Name"], "dq-acpvpn-ec2-linux")
-#
-#     def test_name_prefix_ACPVPNRouteToInternet(self):
-#         self.assertEqual(self.result['acpvpn']["aws_internet_gateway.ACPVPNRouteToInternet"]["tags.Name"], "dq-acpvpn-igw")
-#
-#     def test_name_prefix_BastionsACPVPN(self):
-#         self.assertEqual(self.result['acpvpn']["aws_security_group.BastionsACPVPN"]["tags.Name"], "dq-acpvpn-sg")
-#
-#     def test_name_prefix_acpvpnvpc(self):
-#         self.assertEqual(self.result['acpvpn']["aws_vpc.acpvpnvpc"]["tags.Name"], "dq-acpvpn-vpc")
+
+    def test_vpc_apcvpn_cidr_block(self):
+        self.assertEqual(self.result['mock-acp']["aws_vpc.acpvpnvpc"]["cidr_block"], "10.4.0.0/16")
+
+    def test_subnet_acpvpn_cidr_block(self):
+        self.assertEqual(self.result['mock-acp']["aws_subnet.ACPVPNSubnet"]["cidr_block"], "10.4.1.0/24")
+
+    def test_az(self):
+        self.assertEqual(self.result['mock-acp']["aws_subnet.ACPVPNSubnet"]["availability_zone"], "eu-west-2a")
+
+    def test_name_prefix_BastionHostLinuxACPVPN(self):
+        self.assertEqual(self.result['mock-acp']["aws_instance.BastionHostLinuxACPVPN"]["tags.Name"], "dq-acpvpn-ec2-linux")
+
+    def test_name_prefix_ACPVPNRouteToInternet(self):
+        self.assertEqual(self.result['mock-acp']["aws_internet_gateway.ACPVPNRouteToInternet"]["tags.Name"], "dq-acpvpn-igw")
+
+    def test_name_prefix_BastionsACPVPN(self):
+        self.assertEqual(self.result['mock-acp']["aws_security_group.BastionsACPVPN"]["tags.Name"], "dq-acpvpn-sg")
+
+    def test_name_prefix_acpvpnvpc(self):
+        self.assertEqual(self.result['mock-acp']["aws_vpc.acpvpnvpc"]["tags.Name"], "dq-acpvpn-vpc")
 
 if __name__ == '__main__':
     unittest.main()
