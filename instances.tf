13370cf0cd82f56561f6ad5578fcd93651e80030
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-05 18:31:09 +0000

    use the connectivity tester module

--- instances.tf
+++ instances.tf
@@ -1,23 +1,3 @@
-variable instance_type {
-  default = "t2.nano"
-}
-
-data "aws_ami" "linux_connectivity_tester" {
-  most_recent = true
-
-  filter {
-    name = "name"
-
-    values = [
-      "connectivity-tester-linux*",
-    ]
-  }
-
-  owners = [
-    "093401982388",
-  ]
-}
-
 locals {
   acpcicd_tag = "${var.name_prefix}acpcicd-"
   acpops_tag  = "${var.name_prefix}acpops-"
@@ -27,20 +7,13 @@ locals {
 
 #### ACPCICD
 
-resource "aws_instance" "BastionHostLinuxACPCICD" {
-  ami                    = "${data.aws_ami.linux_connectivity_tester.id}"
-  instance_type          = "${var.instance_type}"
-  subnet_id              = "${aws_subnet.ACPCICDSubnet.id}"
-  vpc_security_group_ids = ["${aws_security_group.BastionsACPCICD.id}"]
-
-  tags {
-    Name = "${local.acpcicd_tag}ec2-linux"
-  }
-
-  user_data = "CHECK_self=127.0.0.1:8080 CHECK_google=google.com:80 CHECK_googletls=google.com:443 LISTEN_http=0.0.0.0:80"
+module "ACPCICD" {
+  source    = "github.com/UKHomeOffice/connectivity-tester-tf"
+  user_data = "CHECK_self=127.0.0.1:80 CHECK_google=google.com:80 CHECK_googletls=google.com:443 LISTEN_http=0.0.0.0:80"
+  subnet_id = "${aws_subnet.ACPCICDSubnet.id}"
 }
 
-resource "aws_security_group" "BastionsACPCICD" {
+resource "aws_security_group" "ACPCICD" {
   vpc_id = "${aws_vpc.acpcicdvpc.id}"
 
   tags {
@@ -51,33 +24,28 @@ resource "aws_security_group" "BastionsACPCICD" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 }
 
 #### ACPOPS
 
-resource "aws_instance" "BastionHostLinuxACPOPS" {
-  ami                    = "${data.aws_ami.linux_connectivity_tester.id}"
-  instance_type          = "${var.instance_type}"
-  subnet_id              = "${aws_subnet.ACPOPSSubnet.id}"
-  vpc_security_group_ids = ["${aws_security_group.BastionsACPOPS.id}"]
-
-  tags {
-    Name = "${local.acpops_tag}ec2-linux"
-  }
-
-  user_data = "CHECK_self=127.0.0.1:8080 CHECK_google=google.com:80 CHECK_googletls=google.com:443 LISTEN_http=0.0.0.0:80"
+module "ACPOPS" {
+  source    = "github.com/UKHomeOffice/connectivity-tester-tf"
+  user_data = "CHECK_self=127.0.0.1:80 CHECK_google=google.com:80 CHECK_googletls=google.com:443 LISTEN_http=0.0.0.0:80"
+  subnet_id = "${aws_subnet.ACPOPSSubnet.id}"
 }
 
-resource "aws_security_group" "BastionsACPOPS" {
+resource "aws_security_group" "ACPOPS" {
   vpc_id = "${aws_vpc.acpopsvpc.id}"
 
   tags {
@@ -88,33 +56,28 @@ resource "aws_security_group" "BastionsACPOPS" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 }
 
 #### ACPVPN
 
-resource "aws_instance" "BastionHostLinuxACPVPN" {
-  ami                    = "${data.aws_ami.linux_connectivity_tester.id}"
-  instance_type          = "${var.instance_type}"
-  subnet_id              = "${aws_subnet.ACPVPNSubnet.id}"
-  vpc_security_group_ids = ["${aws_security_group.BastionsACPVPN.id}"]
-
-  tags {
-    Name = "${local.acpvpn_tag}ec2-linux"
-  }
-
-  user_data = "CHECK_self=127.0.0.1:8080 CHECK_google=google.com:80 CHECK_googletls=google.com:443 LISTEN_http=0.0.0.0:80"
+module "ACPVPN" {
+  source    = "github.com/UKHomeOffice/connectivity-tester-tf"
+  user_data = "CHECK_self=127.0.0.1:80 CHECK_google=google.com:80 CHECK_googletls=google.com:443 LISTEN_http=0.0.0.0:80"
+  subnet_id = "${aws_subnet.ACPVPNSubnet.id}"
 }
 
-resource "aws_security_group" "BastionsACPVPN" {
+resource "aws_security_group" "ACPVPN" {
   vpc_id = "${aws_vpc.acpvpnvpc.id}"
 
   tags {
@@ -125,33 +88,27 @@ resource "aws_security_group" "BastionsACPVPN" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 }
 
 #### ACPPROD
-
-resource "aws_instance" "BastionHostLinuxACPPROD" {
-  ami                    = "${data.aws_ami.linux_connectivity_tester.id}"
-  instance_type          = "${var.instance_type}"
-  subnet_id              = "${aws_subnet.ACPPRODSubnet.id}"
-  vpc_security_group_ids = ["${aws_security_group.BastionsACPPROD.id}"]
-
-  tags {
-    Name = "${local.acpprod_tag}ec2-linux"
-  }
-
-  user_data = "CHECK_self=127.0.0.1:8080 CHECK_google=google.com:80 CHECK_googletls=google.com:443 LISTEN_http=0.0.0.0:80"
+module "ACPPROD" {
+  source    = "github.com/UKHomeOffice/connectivity-tester-tf"
+  user_data = "CHECK_self=127.0.0.1:80 CHECK_google=google.com:80 CHECK_googletls=google.com:443 LISTEN_http=0.0.0.0:80"
+  subnet_id = "${aws_subnet.ACPPRODSubnet.id}"
 }
 
-resource "aws_security_group" "BastionsACPPROD" {
+resource "aws_security_group" "ACPPROD" {
   vpc_id = "${aws_vpc.acpprodvpc.id}"
 
   tags {
@@ -162,13 +119,15 @@ resource "aws_security_group" "BastionsACPPROD" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
 }
