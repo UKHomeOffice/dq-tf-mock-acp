469fbaa8172aff0b9059ea0d066f9ec26eebd32c
david-wirelab <info@wirelab.co.uk>
2017-12-14 16:22:07 +0000

    cicd routing

--- variables.tf
+++ variables.tf
@@ -8,3 +8,11 @@ variable "acpvpn_cidr_block" {}
 variable "acpvpn_vpc_subnet_cidr_block" {}
 variable "az" {}
 variable "name_prefix" {}
+
+variable "route_table_cidr_blocks" {
+  type = "map"
+}
+
+variable "vpc_peering_connection_ids" {
+  type = "map"
+}
