be6efd07c6bce3af8107edb939f2f7b1dac88faf
david-wirelab <info@wirelab.co.uk>
2017-12-15 11:54:47 +0000

    vpn routing

--- acpvpn.tf
+++ acpvpn.tf
@@ -10,15 +10,7 @@ resource "aws_vpc" "acpvpnvpc" {
   }
 }
 
-resource "aws_route_table" "acpvpn_route_table" {
-  vpc_id = "${aws_vpc.acpvpnvpc.id}"
-
-  tags {
-    Name = "${local.acpvpn_name_prefix}route-table"
-  }
-}
-
-resource "aws_subnet" "ACPVPNSubnet" {
+resource "aws_subnet" "acpvpn_subnet" {
   vpc_id                  = "${aws_vpc.acpvpnvpc.id}"
   cidr_block              = "${var.acpvpn_vpc_subnet_cidr_block}"
   map_public_ip_on_launch = true
@@ -27,4 +19,40 @@ resource "aws_subnet" "ACPVPNSubnet" {
   tags {
     Name = "${local.acpvpn_name_prefix}subnet"
   }
+}
+
+resource "aws_internet_gateway" "acpvpn_igw" {
+  vpc_id = "${aws_vpc.acpvpnvpc.id}"
+
+  tags {
+    Name = "${local.acpvpn_name_prefix}igw"
+  }
+}
+
+resource "aws_route_table" "acpvpn_route_table" {
+  vpc_id = "${aws_vpc.acpvpnvpc.id}"
+
+  route {
+    cidr_block                = "${var.route_table_cidr_blocks["ops_cidr"]}"
+    vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_acpvpn"]}"
+  }
+
+  route {
+    cidr_block                = "${var.route_table_cidr_blocks["apps_cidr"]}"
+    vpc_peering_connection_id = "${var.vpc_peering_connection_ids["ops_and_acpvpn"]}"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.acpvpn_igw.id}"
+  }
+
+  tags {
+    Name = "${local.acpvpn_name_prefix}route-table"
+  }
+}
+
+resource "aws_route_table_association" "acpvpn_rt_assocation" {
+  subnet_id      = "${aws_subnet.acpvpn_subnet.id}"
+  route_table_id = "${aws_route_table.acpvpn_route_table.id}"
 }
